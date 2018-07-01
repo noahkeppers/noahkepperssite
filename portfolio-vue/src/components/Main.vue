9a45d037a3291e6210c1b718afd61ac9fe00b07a
Noah Keppers <keppersn@msu.edu>
2018-07-01 13:42:01 -0400

    Create template vue.js app

---
+++ portfolio-vue/src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <img src="../assets/me.jpeg">
+    <h1>Noah Keppers</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
