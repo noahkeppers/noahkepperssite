f0a6c5a1858f3e9785b5995e643a206bbc30444f
Noah Keppers <nkeppers@gmail.com>
2018-09-06 22:15:29 -0400

    Fix staggered animation bug

--- src/mixins/transitions.js
+++ src/mixins/transitions.js
@@ -11,7 +11,7 @@ const transitionsMixin = { methods:
 
       setTimeout(() => {
         element.style.height = height
-      })
+      }, 1000)
     },
     afterEnter (element) {
       element.style.height = 'auto'
